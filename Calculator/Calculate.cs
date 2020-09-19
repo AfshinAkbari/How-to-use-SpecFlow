1cf2b5cf2b8e14f62a777a0b8b063558a79dda9c
Afshin <Afsin.669@gmail.com>
2020-09-19 10:03:22 +0430

    init

---
+++ Calculator/Calculate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculate
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
